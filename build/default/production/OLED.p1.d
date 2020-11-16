f6ebfe88f91f24413351fe490f8219f2d5a87d9f
dklepps <dklepps@raffel.com>
2020-11-16 12:44:17 -0600

    Initial commit

---
+++ build/default/production/OLED.p1.d
@@ -0,0 +1,6 @@
+build/default/production/OLED.p1:  \
+OLED.c  \
+mcc_generated_files/device_config.h  \
+main.h  \
+OLED.h  \
+I2C.h 
---
+++ main.c
@@ -0,0 +1,98 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.6
+        Device            :  PIC16F1936
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "OLED.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+
+    // initialize the device
+    SYSTEM_Initialize();
+
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+    __delay_ms(100);
+    OLED_Init();
+    uint8_t lineIterator = 0;
+    uint8_t lineNumber = 0;
+    uint8_t textIterator = 0;
+    char RAFTEXT[15] = {'R','a','f','f','e','l',' ','S','y','s','t','e','m','s',' '};
+    while (1)
+    {
+        OLED_Write(lineIterator*12,lineNumber*16,RAFTEXT[textIterator]);
+        OLED_Update();
+        lineIterator++;
+        if(lineIterator>9){
+            lineIterator = 0;
+            lineNumber++;
+            if(lineNumber>3){
+                lineNumber=0;
+            }
+        }
+        textIterator++;
+        if(textIterator>14){
+            textIterator = 0;
+        }
+    }
+}
+
+/**
+ End of File
+*/
